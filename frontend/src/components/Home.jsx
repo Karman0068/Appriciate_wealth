721757724bd81273e2303fda287a04a1ae373f84
Karman0068 <karman0068@gmail.com>
2024-09-15 23:32:20 +0530

    1st

---
+++ frontend/src/components/Home.jsx
@@ -0,0 +1,32 @@
+// import React from 'react';
+import { Link } from "react-router-dom";
+
+const Home = () => {
+  return (
+    <>
+    <p className="text-white">gg</p>
+    <p className="text-white">gg</p>
+    <div className="h-72 w-72"> 
+      <img className="h-36 w-72" src="https://hbkonline.in/pub/media/catalog/product/a/p/apple_fruit_powder3.jpg"/>
+      An apple a day keeps doctor away
+    </div>
+
+    <div className="h-72 w-72"> 
+      <img className="h-36 w-72" src="https://5.imimg.com/data5/EE/ER/MY-27568370/fresh-orange-500x500.png"/>
+      orange is good for health
+    </div>
+
+    <div className="h-72 w-72"> 
+      <img className="h-36 w-72" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRYfbArIFM3Ej5ovR2fsSiBxEFrq-DFeoole2nzsEOXplQLJHICfxUvStIjADXCV7HZU40&usqp=CAU"/>
+      strawberry is tasty
+    </div>
+
+    <div className="h-72 w-72"> 
+      <img className="h-36 w-72" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT5insVD9zlFci0Lg3uhGLOVp_UCLBoELBUszrD8aKPPcKic4jqUaQvGyl4mxhBbjiCGwE&usqp=CAU"/>
+      pineapple is good for health
+    </div>
+    </>
+  )
+}
+
+export default Home
